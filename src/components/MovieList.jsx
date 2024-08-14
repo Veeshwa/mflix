8be1de531b45832d16449fe8527297e9b2eb85cb
full-stack-dhivakar <106915835+full-stack-dhivakar@users.noreply.github.com>
2024-08-14 17:47:02 +0530

    Layout-done

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,6 +1,6 @@
 import { getMovies } from "../lib/get-movies";
 import { useEffect, useState } from "react";
-import Movie from "./Movie"
+import { Card, CardBody, CardFooter,Stack,ButtonGroup,Image,Heading,Text,Divider,Button ,Grid} from '@chakra-ui/react'
 
 function MovieList() {
     const [movieList , setMovieList] = useState([]);
@@ -11,16 +11,50 @@ function MovieList() {
     }, []);
     return ( 
     <>
-        <h1>Movie List</h1>
-        <ul>
+    
+        <Heading fontSize='50px' textAlign="center" color='red.600'>M flix</Heading>
+        <Grid templateColumns='repeat(3, 1fr)' gap={6}>
+        {/* <SimpleGrid spacing={3} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'> */}
         {
-            movieList.map((movie, index) =>(
-                <li key={index}>
-                    <Movie movie={movie}></Movie>
-                </li>
+            
+            movieList.map((movie,index) =>(
+               
+                     <Card key={index} maxW='sm'>
+  <CardBody>
+    <Image
+    src=""
+      alt='Comming soon!'
+      borderRadius='lg'
+    />
+    <Stack mt='6' spacing='3'>
+      <Heading size='md'>{movie.title}</Heading>
+      <Text>
+        {movie.overview}
+      </Text>
+      <Text color='red.600' fontSize='2xl'>
+        $1299 
+      </Text>
+    </Stack>
+  </CardBody>
+  <Divider />
+  <CardFooter>
+    <ButtonGroup spacing='3'>
+      <Button variant='solid' colorScheme='red'>
+        Watch
+      </Button>
+      <Button variant='ghost' colorScheme='red'>
+       Rent
+      </Button>
+    </ButtonGroup>
+  </CardFooter>
+</Card>
+                    
+                
             ))
         }
-        </ul>
+       </Grid>
+
+       
         
     </>
      );
